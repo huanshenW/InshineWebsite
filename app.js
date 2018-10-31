343b4636b5e22e367fb4099c18e7037c801a0bea
Huanshen Wei <huanshew@usc.edu>
2018-10-31 05:48:27 +0000

    change mongolab server address

--- app.js
+++ app.js
@@ -17,8 +17,8 @@ var
     manageRoutes            = require("./routes/manage"),
     homeRoutes              = require("./routes/home")
 
-mongoose.connect("mongodb://localhost:27017/anxin", { useNewUrlParser: true });
-// mongoose.connect("mongodb://huanshen:wilson123@ds051833.mlab.com:51833/yelpcamp", { useNewUrlParser: true });
+// mongoose.connect("mongodb://localhost:27017/anxin", { useNewUrlParser: true });
+mongoose.connect("mongodb://huanshew:axjy#0205#@ds147003.mlab.com:47003/inshine", { useNewUrlParser: true });
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
